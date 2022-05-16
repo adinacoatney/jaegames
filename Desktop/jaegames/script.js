e83dc98518a8c7f231dfe2937942ff4b9646b5b9
Adina Coatney <adinacoatney@hotmail.com>
2022-05-16 17:09:55 -0500

    Updated Instructions Header and Colors

---
+++ Desktop/jaegames/script.js
@@ -0,0 +1,34 @@
+var test = document.getElementById("test");
+
+var text = "Yellow Brick Road"; //put your text here
+var result = "";
+//Press rerun button to see effect
+window.addEventListener("load", (event) => {
+  for (let i = 0; i < text.length; i++) {
+    setTimeout(function () {
+      result += text[i];
+      test.innerHTML = result;
+    }, 180 * i);
+  }
+});
+
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById('myImg');
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
